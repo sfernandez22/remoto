837e60ff246818687af53b4a6d07306851291e47
Samuel <sfernandez@nelson.local>
2022-04-20 10:55:54 +0200

    Creada la version 1.0

---
+++ src/git5/Main.java
@@ -0,0 +1,26 @@
+package git5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Scanner sn = new Scanner(System.in);
+		
+		System.out.println("Dime tu nombre: ");
+		String nombre = sc.nextLine();
+		System.out.println("Dime tus apellidos: ");
+		String apellidos = sc.nextLine();
+		System.out.println("Dame tu fecha de nacimineto: ");
+		String fecha_nacimineto = sc.nextLine();
+		System.out.println("Dame tu DNI: ");
+		String dni = sc.nextLine();
+		System.out.println("Dime tus estudios previos: ");
+		String estudios_previos = sc.nextLine();
+		System.out.println("Dime tu telefono: ");
+		String telefono = sc.nextLine();
+	}
+
+}
